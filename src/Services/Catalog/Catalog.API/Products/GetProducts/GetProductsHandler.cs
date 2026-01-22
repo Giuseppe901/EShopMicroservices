df1f7f8d80245387a02122486f9f2728b00a58e2
GiuseppeR <g.ranieri@degroup.it>
2026-01-22 17:40:19 +0100

    -- integrazione docker postgres

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,21 @@
+﻿
+using Marten.Linq;
+
+namespace Catalog.API.Products.GetProducts;
+using Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal record GetProductsQuery() : IQuery<GetProductsResult>;
+internal record GetProductsResult(IEnumerable<Product> Products);
+internal class GetProductsHandler (IDocumentSession session, ILogger<GetProductsHandler> logger)
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsHandler.Handle called with {@Query}", query);
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
+    }
+}
+
