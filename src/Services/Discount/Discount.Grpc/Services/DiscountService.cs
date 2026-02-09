03d44b6fe4e6b93db1ff19e5d00925711e45538d
GiuseppeR <g.ranieri@degroup.it>
2026-02-09 09:33:43 +0100

    add discount grpc

--- src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -11,26 +11,19 @@ public class DiscountService(DiscountContext dbContext, ILogger<DiscountService>
 {
     public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
     {
-        try
-        {
-            var coupon = await dbContext
-                .Coupons
-                .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
 
-            if (coupon is null)
-                coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+        var coupon = await dbContext
+            .Coupons
+            .FirstOrDefaultAsync(x => x.ProductName == request.ProductName);
 
-            logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
+        if (coupon is null)
+            coupon = new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
 
-            var couponModel = coupon.Adapt<CouponModel>();
-            return couponModel;
-        }
-        catch (Exception ex)
-        {
+        logger.LogInformation("Discount is retrieved for ProductName : {productName}, Amount : {amount}", coupon.ProductName, coupon.Amount);
 
-        }
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
 
-        return null;
     }
     public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
     {
